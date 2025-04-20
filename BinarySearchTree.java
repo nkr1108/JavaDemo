fb438536341b5f7c50502658034cace8e8b64d9f
Nirmal Kumar <38359383+nkr1108@users.noreply.github.com>
2025-04-20 17:09:20 -0500

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,47 @@
+class BinarySearchTree {
+
+    class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    BinarySearchTree() {
+        root = null;
+    }
+
+    void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    Node insertRec(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+        return root;
+    }
+
+    boolean search(int target) {
+        return searchRec(root, target);
+    }
+
+    private boolean searchRec(Node node, int target) {
+        if (node == null) return false;
+        if (node.data == target) return true;
+        if (target < node.data) return searchRec(node.left, target);
+        else return searchRec(node.right, target);
+    }
+
+}
